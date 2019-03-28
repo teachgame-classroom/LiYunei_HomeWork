9c5e3dcb126a807453745360ec7cd76af6bec715
1046408786 <1046408786@qq.com>
2019-03-28 23:05:14 +0800

    190328 homework

---
+++ Unity Homework/Assets/Scenes/19_03_28 Homework/PlayerMove.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody body;
+
+    public int score;
+    public int level;
+
+    public float speed = 10;
+    public float jumpWeight = 100;
+
+    public bool colloider = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        body.AddForce((Vector3.forward * vertical + Vector3.right * horizontal) * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            body.AddForce(Vector3.up*jumpWeight);
+        }
+        
+    }
+
+    private void AddScore()
+    {
+        score += 1;
+
+        Debug.Log("+1");
+
+        if(score % 5 == 0&&score != 0)
+        {
+            level += 1;
+            Debug.Log("LevelUp");
+        }
+    }
+
+    private bool OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "Score")
+        {
+            AddScore();
+
+            return colloider = true;
+        }
+        else
+        {
+            return colloider = false;
+        }
+       
+    }
+}
---
+++ Unity Homework/Assets/Scenes/19_03_28 Homework/UseFileInUnity.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+public class UseFileInUnity : MonoBehaviour
+{
+    public SaveData data;
+
+    private PlayerMove player;
+
+    string savePath;
+    string[] lines;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        savePath = Application.persistentDataPath + "/" + "score.txt";
+
+        if (File.Exists(savePath))
+        {
+            lines = File.ReadAllLines(savePath);
+
+            ScoreInfo[] infos = new ScoreInfo[lines.Length];        //生成多少infos数组
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                infos[i] = new ScoreInfo(lines[i]);                 //把每一行放进infos里拆开
+            }
+
+            SaveData data = new SaveData();
+            data.infos = infos;                                     //把更新后的infos放到score里
+
+            SaveData Data = (SaveData)Load(savePath);
+        }
+        else
+        {
+            Save(data, savePath);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.Find("Player");
+        PlayerMove playerMove = player.GetComponent<PlayerMove>();
+
+        if (playerMove.colloider)
+        {
+            data.infos[0] = new ScoreInfo(playerMove.score, playerMove.level);
+            Save(data, savePath);
+        }
+    }
+
+    public void Save(object data, string path)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+
+        FileStream file = File.Create(path);
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+
+    public object Load(string path)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream readStream = File.Open(path, FileMode.Open);
+        object ret = (SaveData)bf.Deserialize(readStream);
+
+        readStream.Close();
+
+        return ret;
+    }
+}
