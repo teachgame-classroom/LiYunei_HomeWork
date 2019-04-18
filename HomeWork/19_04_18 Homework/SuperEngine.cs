71bbe4ba4d614b477c6ce46e11c476b92b920bc1
1046408786 <1046408786@qq.com>
2019-04-18 16:28:58 +0800

    Updata 190418 homework

---
+++ HomeWork/19_04_18 Homework/SuperEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_04_18_Homework
+{
+    class SuperEngine : CarEngine
+    {
+        public void Turbo()
+        {
+            Console.WriteLine("Turbocharged super engine");
+        }
+
+        public override void TurnOn()
+        {
+            Console.WriteLine("Run super engine");
+        }
+
+        public override void TurnOff()
+        {
+            Console.WriteLine("Colse super engine");
+        }
+    }
+}
